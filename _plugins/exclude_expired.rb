c1316608ad236f8437b75f32777ca9cfed2750d9
Spencer Elliott <me@elliottsj.com>
2014-09-07 13:59:28 -0400

    #11 Reverse the comparison thing

--- _plugins/exclude_expired.rb
+++ _plugins/exclude_expired.rb
@@ -6,7 +6,7 @@ module ExcludeExpired
       # Delete posts if their `expiry_date` in their frontmatter is today or later
       site.posts.delete_if do |post|
         expiry_date = post.data['expiry_date']
-        expiry_date >= Date.today if expiry_date
+        expiry_date <= Date.today if expiry_date
       end
 
     end
