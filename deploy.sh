6d61a73cb107e4680a47dc90c8b52f086ad059a3
Spencer Elliott <me@elliottsj.com>
2016-09-29 13:10:36 -0400

    Revert "Deploy to GH Pages"
    b5711839c34d77c605d585588537dbd1e0d7fc7e

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,12 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+# Add dbsrv1 to known_hosts
+echo "Adding dbsrv1.cdf.toronto.edu to ~/.ssh/known_hosts..."
+cat << EOF >> ~/.ssh/known_hosts
+dbsrv1.cdf.toronto.edu ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAIEAydP6bdmyTALluMTn4xEs+Fu8OAFDbyV93owVVNGxerSvNijXRt7BuvaCY13mXtCyJ2BsDzNYZ5kR/+FFCIv5e6/3Y5W4yjkwRZ5/Drt5ZK2fJtmeRYyVOztQHSyWIY9KBJmrcGpDPpK6LY6kseR2djWBfwGLCejSKXWsZ5+Oz/0=
+EOF
+
 # Decrypt deploy key
 echo "Decrypting deploy key to .travis/deploy_key..."
 openssl aes-256-cbc -K $encrypted_d7c771f4f1c5_key -iv $encrypted_d7c771f4f1c5_iv -in .travis/deploy_key.enc -out .travis/deploy_key -d
@@ -13,8 +19,14 @@ eval "$(ssh-agent -s)"
 chmod 600 .travis/deploy_key
 ssh-add .travis/deploy_key
 
-# Deploy to github.com/cssu/cssu#gh-pages
-git remote add deploy git@github.com:cssu/cssu.ca.git
-git add --force _site
-git commit -m "Build static site"
-git subtree push --prefix _site deploy gh-pages
+# Copy the static site to CDF, preserving the gallery and wiki directories
+echo "Copying static site to dbsrv1.cdf.toronto.edu:/space/data/www/cssu/htdocs via rsync..."
+rsync \
+  --archive \
+  --compress \
+  --delete \
+  --exclude="/gallery/" \
+  --exclude="/w/" \
+  --verbose \
+  _site/ \
+  cssuwww@dbsrv1.cdf.toronto.edu:/space/data/www/cssu/htdocs
