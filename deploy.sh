48949c4c36116e979af46f4c2834ca74f53f6f42
Eugene Cheung <ey.cheung@outlook.com>
2017-10-14 01:00:34 -0400

    Use an SSH proxy to rsync to earth

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,11 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-# Add dbsrv1 to known_hosts
+# Add dbsrv1 and earth to known_hosts
 echo "Adding dbsrv1.teach.cs.toronto.edu to ~/.ssh/known_hosts..."
 cat << EOF >> ~/.ssh/known_hosts
 dbsrv1.teach.cs.toronto.edu ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAIEAydP6bdmyTALluMTn4xEs+Fu8OAFDbyV93owVVNGxerSvNijXRt7BuvaCY13mXtCyJ2BsDzNYZ5kR/+FFCIv5e6/3Y5W4yjkwRZ5/Drt5ZK2fJtmeRYyVOztQHSyWIY9KBJmrcGpDPpK6LY6kseR2djWBfwGLCejSKXWsZ5+Oz/0=
+earth.teach.cs.toronto.edu ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAIEAydP6bdmyTALluMTn4xEs+Fu8OAFDbyV93owVVNGxerSvNijXRt7BuvaCY13mXtCyJ2BsDzNYZ5kR/+FFCIv5e6/3Y5W4yjkwRZ5/Drt5ZK2fJtmeRYyVOztQHSyWIY9KBJmrcGpDPpK6LY6kseR2djWBfwGLCejSKXWsZ5+Oz/0=
 EOF
 
 # Decrypt deploy key
@@ -19,28 +20,18 @@ eval "$(ssh-agent -s)"
 chmod 600 .travis/deploy_key
 ssh-add .travis/deploy_key
 
-# Copy the static site to the a temporary location on CS Teaching Labs dbsrv1 server
-echo "Copying static site to dbsrv1.teach.cs.toronto.edu:/data/www/cssu/htdocs via rsync..."
+# Copy the static site to the CS Teaching Labs, preserving the gallery and wiki directories
+# We proxy through dbsrv1 to earth since earth isn't accessible outside of the Teaching Labs network
+# For more info on proxying, see: https://superuser.com/a/1115998
+echo "Copying static site to earth.teach.cs.toronto.edu:/data/www/cssu/htdocs via rsync..."
 rsync \
+  -e 'ssh -o "ProxyCommand ssh -A cssuwww@dbsrv1.teach.cs.toronto.edu -W %h:%p"' \
   --archive \
   --compress \
   --delete \
+  --exclude="/data/" \
+  --exclude="/gallery/" \
+  --exclude="/w/" \
   --verbose \
   _site/ \
-  cssuwww@dbsrv1.teach.cs.toronto.edu:/u/cssuwww/tmpsite
-
-# Copy the files from dbsrv1 to earth (since earth is only accessible from within the CS Teaching Labs network),
-# preserving the gallery and wiki directories
-ssh cssuwww@dbsrv1.teach.cs.toronto.edu /bin/bash << EOF
-  cd /u/cssuwww/tmpsite
-  rsync \
-    --archive \
-    --compress \
-    --delete \
-    --exclude="/data/" \
-    --exclude="/gallery/" \
-    --exclude="/w/" \
-    --verbose \
-    . \
-    cssuwww@earth.teach.cs.toronto.edu:/data/www/cssu/htdocs
-EOF
+  cssuwww@earth.teach.cs.toronto.edu:/data/www/cssu/htdocs
