cb78b3352370c2eb3fa73a6f78131dacc0b4bfcc
Adam Gordon <adagordon@deloitte.ca>
2018-05-17 21:55:14 -0400

    switch to gh-pages

---
+++ deploy_pages.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+# Strict mode: http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
+
+SOURCE_BRANCH="master"
+TARGET_BRANCH="master"
+TARGET_REPO="git@github.com:cssu/cssu.github.io.git"
+SHA="$(git rev-parse --verify HEAD)"
+
+# Clone the existing gh-pages for this repo into out/
+git clone "$TARGET_REPO" out
+
+# Clean out existing contents
+rm -rf out/**/* || exit 0
+
+# Copy in jekyll site
+cp -r _site/ out/
+
+# Now let's go have some fun with the cloned repo
+cd out
+git config user.name "Travis CI"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
+
+# If there are no changes to the compiled out (e.g. this is a README update) then just bail.
+if git diff --quiet; then
+    echo "No changes to the output on this push; exiting."
+    exit 0
+fi
+
+# Commit the "changes", i.e. the new version.
+# The delta will show diffs between new and old versions.
+git add -A .
+git commit -m "Deploy to GitHub Pages: ${SHA}"
+
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+openssl aes-256-cbc -K $encrypted_79f4b640b727_key -iv $encrypted_79f4b640b727_iv -in .travis/deploy_pages_key.enc -out .travis/deploy_pages_key -d
+
+eval "$(ssh-agent -s)"
+chmod 600 .travis/deploy_pages_key
+ssh-add .travis/deploy_pages_key
+
+# Now that we're all set up, we can push.
+git push "TARGET_REPO" "$TARGET_BRANCH"
