2b2bacd093a6c47fb1d6c00a84f64e694b469444
Eugene Cheung <ey.cheung@outlook.com>
2018-08-29 22:36:13 -0400

    Refactor calendar code

--- _assets/javascripts/calendar.js
+++ _assets/javascripts/calendar.js
@@ -1,95 +1,114 @@
+'use strict';
+
 var API_KEY = 'AIzaSyAZRDe_DuPP4BUS7hqkkdFeAVv_O2yQgIk';
 var CAL_ID = '0fqfmmqnmb20kn6qibaqflsbo0@group.calendar.google.com';
 var SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
 
+var eventsListEl = document.getElementById('events');
+
 /**
-* Load Google Calendar client library. List upcoming events
-* once client library is loaded.
-*/
+ * Load Google Calendar client library. List upcoming events
+ * once client library is loaded.
+ */
 function loadCalendarApi() {
-    window.gapi_onload = function() { gapi.auth.authorize(); };
-    gapi.client.setApiKey(API_KEY);
-    gapi.client.load('calendar', 'v3', listUpcomingEvents);
+  window.gapi_onload = gapi.auth.authorize;
+  gapi.client.setApiKey(API_KEY);
+  gapi.client.load('calendar', 'v3', listUpcomingEvents);
 }
 
 function listUpcomingEvents() {
-    var request = gapi.client.calendar.events.list({
-        calendarId: CAL_ID,
-        timeMin: (new Date()).toISOString(),
-        showDeleted: false,
-        singleEvents: true,
-        maxResults: 4,
-        orderBy: 'startTime'
-    });
+  var request = gapi.client.calendar.events.list({
+    calendarId: CAL_ID,
+    timeMin: (new Date()).toISOString(),
+    showDeleted: false,
+    singleEvents: true,
+    maxResults: 4,
+    orderBy: 'startTime'
+  });
 
-    var element = document.getElementById("events");
+  request.execute(function(resp) {
+    document.getElementById('events--loading').classList.add('hidden');
 
-    element.innerHTML = "";
+    var events = resp.items;
 
-    request.execute(function(resp) {
-        var events = resp.items;
-
-        for (var i = 0; i < events.length; i++) {
-            var event = events[i];
-            if (event.start.dateTime) {
-                // Event with explicit start/end times
-                appendTimeEvent(event);
-            } else {
-                appendAllDayEvent(event);
-            }
+    if (events.length !== 0) {
+      for (var i = 0; i < events.length; i++) {
+        var event = events[i];
+        if (event.start.dateTime) {
+          appendTimeEvent(event);
+        } else {
+          appendAllDayEvent(event);
         }
-        if (events.length === 0) {
-            element.innerHTML = "<li class='empty'>No upcoming events; stay tuned!</li>";
-        }
-    });
+      }
+    } else {
+      document.getElementById('events--loading').classList.add('hidden');
+      document.getElementById('events--empty').classList.remove('hidden');
+    }
+  });
 }
 
+/**
+ * Add an event that has explicit start/end times defined.
+ */
 function appendTimeEvent(event) {
-    var startTime = new Date(event.start.dateTime);
-    var endTime = new Date(event.end.dateTime);
-    var element = document.getElementById("events");
-    var str = "<div class='event-date'><p class='event-month'>" +
-                startTime.toLocaleString("en-us", { month: "short" }) +
-                "</p><p class='event-day'>"+ startTime.getDate() +
-                "</p></div><p class='post-link'>" + event.summary + "</p>";
-    var eventNode = document.createElement("li");
-    eventNode.innerHTML = str;
-    if (event.description != undefined){
-        var des = document.createElement("p");
-        des.appendChild(document.createTextNode(event.description));
-        eventNode.appendChild(des);
-    }
-    if (event.location != undefined){
-        var loc = document.createElement("p");
-        loc.appendChild(document.createTextNode("Location: " + event.location));
-        eventNode.appendChild(loc);
-    }
-
-    var time = document.createElement("p");
-        time.appendChild(document.createTextNode("From " + startTime.toLocaleTimeString('en-US') +
-                                                    " to " + endTime.toLocaleTimeString('en-US') ));
-        eventNode.appendChild(time);
-    element.appendChild(eventNode);
+  var startTime = new Date(event.start.dateTime);
+  var endTime = new Date(event.end.dateTime);
+  var eventEl = _createEventEl(startTime, event.summary, event.description, event.location);
+  eventEl.appendChild(_createElement('p', null,
+      'From ' + startTime.toLocaleTimeString('en-US') +
+      ' to ' + endTime.toLocaleTimeString('en-US')));
+  eventsListEl.appendChild(eventEl);
 }
 
+/**
+ * Add an all-day event.
+ */
 function appendAllDayEvent(event) {
-    var start = new Date(event.start.date);
-    var element = document.getElementById("events");
-    var str = "<div class='event-date'><p class='event-month'>" +
-                start.toLocaleString("en-us", { month: "short" }) +
-                    "</p><p class='event-day'>"+ start.getDate() +
-                    "</p></div><p class='post-link'>" + event.summary + "</p>";
-    var eventNode = document.createElement("li");
-    eventNode.innerHTML = str;
-    if (event.description != undefined){
-        var des = document.createElement("p");
-        des.appendChild(document.createTextNode(event.description));
-        eventNode.appendChild(des);
-    }
-    if (event.location != undefined){
-        var loc = document.createElement("p");
-        loc.appendChild(document.createTextNode("Location: " + event.location));
-        eventNode.appendChild(loc);
-    }
-    element.appendChild(eventNode);
+  var start = new Date(event.start.date);
+  var eventEl = _createEventEl(start, event.summary, event.description, event.location);
+  eventsListEl.appendChild(eventEl);
+}
+
+/**
+ * Helper function to create an event list item.
+ */
+function _createEventEl(date, summary, description, location) {
+  var eventEl = _createElement('li');
+
+  var eventDateEl = _createElement('div', {'class': 'event-date'});
+  eventDateEl.appendChild(_createElement('p', {'class': 'event-month'}, date.toLocaleString('en-US', {month: 'short'})));
+  eventDateEl.appendChild(_createElement('p', {'class': 'event-day'}, date.getDate()));
+  eventEl.appendChild(eventDateEl);
+
+  eventEl.appendChild(_createElement('p', {'class': 'post-link'}, summary));
+
+  if (description != null) {
+    eventEl.appendChild(_createElement('p', null, description));
+  }
+
+  if (location != null) {
+    eventEl.appendChild(_createElement('p', null, 'Location: ' + location));
+  }
+
+  return eventEl;
+}
+
+/**
+ * Helper function to create a DOM element.
+ */
+function _createElement(type, attrs, text) {
+  var el = document.createElement(type);
+
+  if (attrs != null) {
+    Object.keys(attrs).forEach(function(key) {
+      var value = attrs[key];
+      el.setAttribute(key, value);
+    });
+  }
+
+  if (text != null) {
+    el.appendChild(document.createTextNode(text));
+  }
+
+  return el;
 }
